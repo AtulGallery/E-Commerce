d9319157d91e746ae88e623053140c0702a58d9c
Atul <atulpathakdec@gmail.com>
2026-07-05 04:41:55 +0530

    local changes before merging newproj

---
+++ src/main/java/ecomm_Shopping/SampleClassCommitUK.java
@@ -0,0 +1,11 @@
+package ecomm_Shopping;
+
+import org.testng.annotations.Test;
+
+public class SampleClassCommitUK {
+
+	@Test
+	public void changeInMethodBYUK() {
+		System.out.println("this commit works perfectly///");
+	}
+}
