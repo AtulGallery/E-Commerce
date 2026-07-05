a504d93a83fd9ccf0c6ee54d09a7ff71cc9eb82f
Atul <atulpathakdec@gmail.com>
2026-07-05 05:31:52 +0530

    added ThisIsFromAfrica in E-comm_Project

---
+++ src/main/java/ecomm_Shopping/ThisIsFromAfrica.java
@@ -0,0 +1,11 @@
+package ecomm_Shopping;
+
+import org.testng.annotations.Test;
+
+public class ThisIsFromAfrica {
+	@Test
+	public void AfricaWorksFine() {
+		System.out.println("git commit from africa");
+	}
+
+}
